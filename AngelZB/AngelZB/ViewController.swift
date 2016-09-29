3e34e0b0dc6eb4c8a5978e9407f1b087c027b2f3
wuyc <wyc@wycdemac.local>
2016-09-28 18:42:26 -0700

    AngelZB PROJECT is init

---
+++ AngelZB/AngelZB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AngelZB
+//
+//  Created by wyc on 16/9/28.
+//  Copyright © 2016年 wyc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
